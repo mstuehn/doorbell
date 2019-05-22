b68b84984f755e8716595208292190e40d094392
Manuel Stühn <freebsd@justmail.de>
2019-05-22 21:35:12 +0200

    idea how to iterate over available input devices

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@
 #include <sys/types.h>
 #include <err.h>
 
+#include <dirent.h>
+
 #include "doorbell.h"
 #include "mqtt.h"
 
@@ -42,16 +44,12 @@ int main( int argc, char* argv[] )
     Json::Value root;
     Json::Reader rd;
     std::string config_filename = "/usr/local/etc/doorbell/config.json";
-    std::string config_devicename = "/dev/input/event7";
 
     signed char ch;
-    while ((ch = getopt(argc, argv, "d:c:h")) != -1) {
+    while ((ch = getopt(argc, argv, "c:h")) != -1) {
         switch (ch) {
         case 'c':
             config_filename = std::string(optarg);
-            break;
-        case 'd':
-            config_devicename = std::string(optarg);
             break;
         case 'h':
             usage();
@@ -78,22 +76,58 @@ int main( int argc, char* argv[] )
 
     mqtt.add_callback( base_topic+"/cmd/ring", [&bell](uint8_t*msg, size_t len){ bell.ring(); } );
 
-    std::thread evdevpoll([&bell, &config_devicename, &mqtt, &base_topic](){
+    std::thread evdevpoll([&bell, &mqtt, &base_topic](){
             struct libevdev *dev = NULL;
-            int fd;
+            int fd = -1;
             int rc = 1;
+            DIR *eventdir;
+            bool evdev_found = false;
+            const char* dirname = "/dev/input";
 
-            fd = open(config_devicename.c_str(), O_RDONLY);
-            rc = libevdev_new_from_fd(fd, &dev);
-            if (rc < 0) {
-                fprintf(stderr, "Failed to init libevdev (%s)\n", strerror(-rc));
-                exit(1);
-            }
-            printf("Input device name: \"%s\"\n", libevdev_get_name(dev));
-            printf("Input device ID: bus %#x vendor %#x product %#x\n",
-                    libevdev_get_id_bustype(dev),
-                    libevdev_get_id_vendor(dev),
-                    libevdev_get_id_product(dev));
+            do {
+                eventdir = opendir(dirname);
+                if( eventdir ) {
+                    while( 1 )
+                    {
+                        struct dirent* dir = readdir(eventdir);
+                        if( dir == nullptr ){
+                            std::cout << "Noting found, try again in 5s" << std::endl;
+                            sleep(5);
+                            break;
+                        } else if( strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0 ) {
+                            char absname[32];
+                            snprintf(absname, sizeof(absname), "%s/%s", dirname, dir->d_name );
+                            std::cout << "Try to open " << absname << std::endl;
+                            fd = open( absname, O_RDONLY);
+                            rc = libevdev_new_from_fd(fd, &dev);
+                            if (rc < 0) {
+                                fprintf(stderr, "Failed to init libevdev (%s)\n", strerror(-rc));
+                                exit(1);
+                            }
+                            int vendor = libevdev_get_id_vendor(dev);
+                            int product = libevdev_get_id_product(dev);
+                            printf("Input device name: \"%s\"\n", libevdev_get_name(dev));
+                            printf("Input device ID: bus %#x vendor %#x product %#x\n",
+                                    libevdev_get_id_bustype(dev),
+                                    vendor,
+                                    product );
+                            if( vendor == 0x25 && product == 0x1 ){
+                                evdev_found = true;
+                                std::cout << "Found suitable input device" << std::endl;
+                                break;
+                            } else {
+                                libevdev_free( dev );
+                                close(fd);
+                            }
+                        }
+                    }
+                    closedir( eventdir );
+                } else {
+                            fprintf(stderr, "Failed to init libevdev (%s)\n", strerror(-rc));
+                            exit(1);
+                }
+            } while( !evdev_found );
+
             do {
                 struct input_event ev;
                 rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_BLOCKING, &ev);
@@ -103,7 +137,8 @@ int main( int argc, char* argv[] )
                             libevdev_event_type_get_name(ev.type),
                             libevdev_event_code_get_name(ev.type, ev.code),
                             ev.value);
-                    if( ev.type == EV_KEY && ev.value == 1 ){
+                    if( ev.type == EV_KEY && ev.code == KEY_F24 && ev.value == 1 )
+                    {
                         bell.ring();
 
                         Json::StreamWriterBuilder wr;
