95d82c2818fe870a2f6ee16741d4abb5c3149aa1
Manuel Stühn <freebsd@justmail.de>
2021-09-08 13:26:43 +0200

    import new EvDevice class

--- src/evdev.cpp
+++ src/evdev.cpp
@@ -1,32 +1,17 @@
-
 #include "evdev.h"
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <linux/input.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <dirent.h>
-#include <errno.h>
-#include <linux/input.h>
 
-#include <string>
-#include <filesystem>
 #include <iostream>
 
-#define DEV_INPUT_EVENT "/dev/input"
-#define EVENT_DEV_NAME "event"
 
-//static int
-//is_event_device(const struct dirent *dir) {
-//    return strncmp(EVENT_DEV_NAME, dir->d_name, 5) == 0;
-//}
+const std::string EvDevice::DEV_INPUT_EVENT = { "/dev/input" };
 
-static bool does_device_match( int fd, int vendor, int product )
+static void print_driver_version( int fd )
 {
     int version;
-    uint16_t id[4];
-
     if( ioctl(fd, EVIOCGVERSION, &version) ) {
         perror("can't get version");
     } else {
@@ -34,22 +19,44 @@ static bool does_device_match( int fd, int vendor, int product )
                 version >> 16, (version >> 8) & 0xff, version & 0xff);
     }
 
-    if( ioctl(fd, EVIOCGID, id) )
-    {
-        perror("can't retrieve vendor/product");
-        return false;
-    }
+}
+
+void EvDevice::add_callback( uint16_t code, std::function<void(uint16_t)> cb )
+{
+    std::lock_guard<std::mutex> lockGuard(m_Mtx);
+    m_Callbacks[code].push_back( cb );
+}
+
+bool EvDevice::device_match(){
+    uint16_t id[4];
+    if( ioctl(m_Fd, EVIOCGID, id) ) return false;
 
     printf("Input device ID: bus 0x%x vendor 0x%x product 0x%x version 0x%x\n\n",
             id[ID_BUS], id[ID_VENDOR], id[ID_PRODUCT], id[ID_VERSION]);
 
-    return (id[ID_VENDOR] == vendor) && (id[ID_PRODUCT] == product);
+    return (id[ID_VENDOR] == m_Vendor ) && (id[ID_PRODUCT] == m_Product );
 }
 
-std::pair<bool, std::string> scan_devices( uint16_t vendor, uint16_t product )
+std::pair<bool, std::pair<uint16_t, uint16_t>> EvDevice::get_events(uint16_t type)
 {
-    printf("Scanning for compatible Vendor/Product:  0x%04X/0x%04X\n", vendor, product );
+    struct input_event ev;
+    auto size = ::read( m_Fd, &ev, sizeof(struct input_event) );
 
+    if( size < (ssize_t)sizeof(struct input_event) ) {
+        printf("expected %lu bytes, got %li\n", sizeof(struct input_event), size);
+        perror("\nerror reading");
+        return { false, {0,0}};
+    }
+
+    if( type != ev.type ) return { false, {0,0}};
+
+    return { true, {ev.code, ev.value }};
+}
+
+
+std::pair<bool, std::string> EvDevice::open()
+{
+    printf("Scanning for compatible Vendor/Product:  0x%04X/0x%04X\n", m_Vendor, m_Product);
     printf("Available devices:\n");
 
     std::string filename;
@@ -57,41 +64,69 @@ std::pair<bool, std::string> scan_devices( uint16_t vendor, uint16_t product )
         char name[256] = "???";
         const std::string& fname = dir.path();
 
-        int fd = open(fname.c_str(), O_RDONLY);
-        if (fd < 0) continue;
+        m_Fd = ::open(fname.c_str(), O_RDONLY);
+        if( m_Fd < 0 ) continue;
 
-        int result = ioctl(fd, EVIOCGNAME(sizeof(name)), name);
-        if( result < 0 ) { close(fd); continue; }
-        printf("%s:  %s\n", fname.c_str(), name);
+        int result = ioctl( m_Fd, EVIOCGNAME(sizeof(name)), name );
+        if( result < 0 ) { close(m_Fd); continue; }
+        printf( "%s:  %s\n", fname.c_str(), name );
 
-        if( !does_device_match( fd, vendor, product )) { close(fd); continue; }
+        if( !device_match()) { close(m_Fd); continue; }
 
-        printf("Device found: %s \n", fname.c_str() );
-        return {true, fname };
+        printf( "Device found: %s \n", fname.c_str() );
+        return { true, "Device found: " + fname };
     }
 
     return { false, "No devices match configured vendor/product" } ;
 }
 
-bool get_events(int fd, uint16_t type, uint16_t* code, uint16_t* value)
+
+EvDevice::EvDevice( uint16_t vendor, uint16_t product ) :
+    m_Vendor( vendor ), m_Product( product ), m_KeepRunning( true ),
+    m_Worker( [this](){
+        while( m_KeepRunning ) {
+            auto result = open();
+            if( result.first ) {
+                std::cout << "Device found, begin monitoring" << std::endl;
+                break;
+            }
+            else {
+                std::cout << "Scanning devices not successfull: " << std::endl;
+                std::cout << ">>" << result.second << "<<" << std::endl;
+                std::cout << "Rescan after 5 seconds" << std::endl;
+                sleep(5);
+            }
+        };
+
+        while( m_KeepRunning ) {
+            auto result = get_events( EV_KEY );
+            const uint16_t code = result.second.first;
+            const uint16_t value = result.second.second;
+
+            if( result.first )
+            {
+                std::lock_guard<std::mutex> lockGuard(m_Mtx);
+                if( !m_Callbacks.count(code) && m_Callbacks[code].empty() ) continue;
+
+                for( auto& callback : m_Callbacks[code] ) {
+                    callback( value );
+                }
+            }
+        }
+        std::cout << "Exit monitor thread" << std::endl;
+    })
 {
-    struct input_event ev;
-    auto size = read(fd, &ev, sizeof(struct input_event));
 
-    if( size < (ssize_t)sizeof(struct input_event) ) {
-        printf("expected %lu bytes, got %li\n", sizeof(struct input_event), size);
-        perror("\nerror reading");
-        return false;
-    }
+}
 
-    //printf("Event: time %ld.%06ld, ", ev.time.tv_sec, ev.time.tv_usec);
-    //printf("type: %i, code: %i, value: %i\n", ev.type, ev.code, ev.value);
+void EvDevice::stop()
+{
+    m_KeepRunning = false;
+    m_Worker.join();
+}
 
-    if( type != ev.type ) return false;
-
-    *code = ev.code;
-    *value = ev.value;
-
-    return true;
+EvDevice::~EvDevice()
+{
+    stop();
 }
 
