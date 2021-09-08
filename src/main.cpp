8b88415a688cfde4088e3020026c88eaa329413e
Manuel Stühn <freebsd@justmail.de>
2021-09-08 10:55:52 +0200

    fix indentation

--- src/main.cpp
+++ src/main.cpp
@@ -87,63 +87,63 @@ int main( int argc, char* argv[] )
             bell.ring();
             } );
 
-            std::thread evdevpoll( [&bell, &mqtt, &base_topic, &root ](){
+    std::thread evdevpoll( [&bell, &mqtt, &base_topic, &root ](){
 
-            uint32_t vendor_number = std::stol(root["input"]["vendor"].asString(), nullptr, 0);
-            uint32_t product_number = std::stol(root["input"]["product"].asString(), nullptr, 0);
+        uint32_t vendor_number = std::stol(root["input"]["vendor"].asString(), nullptr, 0);
+        uint32_t product_number = std::stol(root["input"]["product"].asString(), nullptr, 0);
 
-            std::string filename;
-            do {
-                auto result = scan_devices(vendor_number, product_number);
-                if( result.first ) {
-                    filename = result.second;
-                    break;
-                }
-                else {
-                    std::cout << "Scanning devices not successfull: " << std::endl;
-                    std::cout << ">>" << result.second << "<<" << std::endl;
-                    std::cout << "Rescan after 5 seconds" << std::endl;
-                    sleep(5);
-                }
-            } while( true );
-
-            std::cout << "Opening " << filename << std::endl;
-
-            int fd;
-            if( (fd = open(filename.c_str(), O_RDONLY) ) < 0) {
-                perror("");
-                if (errno == EACCES && getuid() != 0) {
-                    fprintf(stderr, "You do not have access to %s. Try "
-                            "running as root instead.\n", filename.c_str());
-                    }
-                    return;
+        std::string filename;
+        do {
+            auto result = scan_devices(vendor_number, product_number);
+            if( result.first ) {
+                filename = result.second;
+                break;
             }
+            else {
+                std::cout << "Scanning devices not successfull: " << std::endl;
+                std::cout << ">>" << result.second << "<<" << std::endl;
+                std::cout << "Rescan after 5 seconds" << std::endl;
+                sleep(5);
+            }
+        } while( true );
 
-            while( 1 ) {
-                uint16_t code, value;
-                if( get_events( fd, EV_KEY, &code, &value ) && value == 1 )
-                {
-                    switch( code ) {
-                        case KEY_F24:
-                        {
-                            bell.ring();
-                            Json::StreamWriterBuilder wr;
-                            Json::Value info;
-                            info["date"] = now();
-                            info["doorbell"] = true;
-                            std::string msg = Json::writeString(wr, info);
-                            mqtt.publish(base_topic+"/ringed", msg.c_str(), msg.length(), 0 );
-                        } break;
+        std::cout << "Opening " << filename << std::endl;
 
-                        case KEY_F23:
-                        {
-                            using std::chrono::system_clock;
-                            std::time_t now = system_clock::to_time_t( system_clock::now() );
-                            struct std::tm *pnow = std::localtime(&now);
-                            std::cout << std::put_time( pnow, "[%F %T] " ) << "Lifesign" << std::endl;
-                        }break;
+        int fd;
+        if( (fd = open(filename.c_str(), O_RDONLY) ) < 0) {
+            perror("");
+            if (errno == EACCES && getuid() != 0) {
+                fprintf(stderr, "You do not have access to %s. Try "
+                        "running as root instead.\n", filename.c_str());
+                }
+                return;
+        }
 
-                    }
+        while( 1 ) {
+            uint16_t code, value;
+            if( get_events( fd, EV_KEY, &code, &value ) && value == 1 )
+            {
+                switch( code ) {
+                    case KEY_F24:
+                    {
+                        bell.ring();
+                        Json::StreamWriterBuilder wr;
+                        Json::Value info;
+                        info["date"] = now();
+                        info["doorbell"] = true;
+                        std::string msg = Json::writeString(wr, info);
+                        mqtt.publish(base_topic+"/ringed", msg.c_str(), msg.length(), 0 );
+                    } break;
+
+                    case KEY_F23:
+                    {
+                        using std::chrono::system_clock;
+                        std::time_t now = system_clock::to_time_t( system_clock::now() );
+                        struct std::tm *pnow = std::localtime(&now);
+                        std::cout << std::put_time( pnow, "[%F %T] " ) << "Lifesign" << std::endl;
+                    }break;
+
+                }
             }
         }
     });
